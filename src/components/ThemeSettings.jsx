f2696fa0c9376c75c8ce8081cb8c0dc476d9ac29
Mbasa Guzana <mbasaguzana@gmail.com>
2022-07-05 02:52:00 +0200

    :hankey: Update with errors(becuase of bad laptop

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsCheck } from 'react-icons/bs';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { themeColors } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const ThemeSettings = () => {
   return (
-    <div>ThemeSettings</div>
+    <div className='bg-hald-transparent w-screen fixed nav-items top-0 right-0'>
+      <div className='float-right h-screen dark:text-gray-200 bg-white dark:[#484B52]
+      w-400'>
+        <div className='flex justfy-between items-center p-4 ml-4'>
+          <p className='font-semibold text-xl'>Settings</p>
+          <button type='button' onClick={() => {}} style={{ color: 'rgb(153, 171, 100)',
+            borderRadius: '50%' }}
+            className='text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray'
+            >
+          </button>
+        </div>
+
+        <div className='flex-col border-t-1 border-color p-4 ml-4'>
+          <p className='font-semibold text-lg'>
+            Theme Options
+          </p>
+          <div className='mt-4'>
+            <input type='radio' id='light' name='theme' value='Light'
+            className='cursor-pointer' onChange={() => ()}         
+            
+            />
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, CoumnDirective, Page, Search, Inject, Toolbar } from '@syncfusion/ej2-react-grids';
 
-const Employees = () => {
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
+const Employess = () => {
   return (
-    <div>Employees</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Employees'
+      />
+      <GridComponent dataSource={employeesData}
+      allowPaging allowSorting toolbar={['Search']} 
+      width='auto'> 
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnsDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject service={[Page, Search, Toolbar]} />  
+      </GridComponent>
+    </div>
   )
 }
 
-export default Employees
+export default Employess
